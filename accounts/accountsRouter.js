ab1e3fc7b3fa50f4dd2df4a7a829f3282467d998
danRolon <daniel.rolon@yahoo.com>
2019-10-07 17:37:33 -0500

    mvp done

---
+++ accounts/accountsRouter.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../data/dbConfig.js');
+
+
+router.get('/', (req, res) => {
+  db('accounts')
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: 'Could not retrieve the list of accounts' });
+    });
+});
+
+
+router.get('/:id', (req, res) => {
+  db('accounts')
+    .where({id: req.params.id})
+    .first()
+    .then(account => {
+      if (account) {
+        res.status(200).json(account);
+      } else {
+        res.status(404).json({ message: 'Account not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/', (req, res) => {
+  const accountData = req.body;
+  // validate
+  if(!accountData.name || !accountData.budget){
+    res.status(400).json({errorMessage:'PLease provide a name and budget.'})
+  }
+  db('accounts')
+    .insert(accountData, 'id')
+    .then(ids => {
+      res.status(200).json(ids);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: 'Account does not exist' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
--- queries.md
+++ queries.md
@@ -32,7 +32,7 @@ set PostalCode = '11122'
 where CustomerID = 92
 
 ## (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-SELECT COUNT(Distinct Country)
+SELECT COUNT(Distinct City)
 FROM Customers;
 
 ## (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
